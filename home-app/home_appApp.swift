2a8f7f49b8e2459a372b030d4fbaea3c2d017f03
stedemicheli <stefano_de@hotmail.com>
2021-11-13 16:22:13 +0700

    Adds xcode project.

---
+++ home-app/home_appApp.swift
@@ -0,0 +1,20 @@
+//
+//  home_appApp.swift
+//  home-app
+//
+//  Created by Stefano on 13.11.21.
+//
+
+import SwiftUI
+
+@main
+struct home_appApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
